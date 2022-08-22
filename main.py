8a2516d123812117dfc5902f798c40421072bd3b
projectperfect <jprfct@protonmail.com>
2022-08-21 19:45:04 -0500

    entries completed

--- main.py
+++ main.py
@@ -24,5 +24,13 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="password")
 password_label.grid(row=3, column=0)
 
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
 
 window.mainloop()
